265972a6053be73c2b0d32f304d1b2eebcc4914e
Peng Cheng <woshichengpeng@gmail.com>
2021-10-14 21:17:05 +0800

    HBB

--- parse.ts
+++ parse.ts
@@ -1,75 +1,81 @@
-import { DOMParser } from 'https://cdn.skypack.dev/@xmldom/xmldom?dts';
-import * as xpath from 'https://cdn.skypack.dev/xpath?dts';
+import { DOMParser } from "https://cdn.skypack.dev/@xmldom/xmldom?dts";
+import * as xpath from "https://cdn.skypack.dev/xpath?dts";
+import { encodeUrl } from "https://deno.land/x/encodeurl/mod.ts";
 
-interface IResult {
-    lastUpdated: Date;
-    aircraft: IAircraft[];
+interface IStock {
+  name: string;
+  type: string;
+  id: number;
+  order: string;
 }
 
-interface IAircraft {
-    registration: string;
-    type: string;
-    serialNumber: string;
-    owner: string;
-    operator: string;
-    lastSeen: Date;
+interface IOrder {
+  stocks: IStock[];
+  order: string;
 }
 
 (async () => {
-    const html = await Deno.readTextFile('./data.html');
-    const doc = new DOMParser().parseFromString(html);
+  const text = Deno.readTextFileSync("./data.json");
+  const lastOrder: IOrder = JSON.parse(text);
 
-    const table = xpath.select('.//table[ contains( ., "updated" ) ]', doc)[0] as Node;
+  const html = await Deno.readTextFile("./data.html");
+  const doc = new DOMParser().parseFromString(html);
 
-    // get the last updated date
-    const updatedDateCell = xpath.select('.//td[ contains( ., "updated" ) ]', table)[0] as Node;
-    const updatedDateContent = updatedDateCell.toString();
+  const section = xpath.select(
+    './/section[ contains( ., "最近调仓" ) ]',
+    doc,
+  )[0] as Node;
+  // get the last updated date
+  const orderContent =
+    (xpath.select('.//p[ contains( ., "温度" ) ]', section)[0] as Node)
+      .textContent!.trim();
 
-    // some crazy parsing to get just the date
-    const updatedDate = updatedDateContent.split(':').map((c) => c.trim()).map((c) => c.split(/\s/));
-    console.log('updated: ', updatedDate[1][0]);
+  if (orderContent == lastOrder.order) {
+    console.log("not update");
+    return;
+  }
 
-    // the total doesn't appear to be accurate
-    // get the total number of aircraft
-    // const totalCell = xpath.select('.//td[ contains( ., "total" ) ]', (table as Node));
-    // const totalContent = (totalCell[0] as Node).toString();
+  const table = xpath.select(
+    './/table[ contains( ., "份" ) ]',
+    section,
+  )[0] as Node;
+  const rows = xpath.select(".//tr", table) as Node[];
 
-    // console.log('total', totalContent);
+  const stocks: IStock[] = [];
+  for (const row of rows) {
+    // /html/body/div[2]/div[2]/div/section[3]/table/tbody/tr[1]/td[1]/div[1]
+    const td1 = (xpath.select("./td[1]", row)[0] as Node);
+    const td2 = (xpath.select("./td[2]", row)[0] as Node);
+    const name = (xpath.select("./div[1]", td1)[0] as Node).textContent!.trim();
+    const type = (xpath.select("./div[2]/div[1]", td1)[0] as Node).textContent!
+      .trim();
+    const id: number =
+      +((xpath.select("./div[2]/div[2]", td1)[0] as Node).textContent!.trim());
+    const order = td2.textContent!.trim();
 
-    // get all but the first two rows, which contain our actual data
-    const rows = xpath.select('.//tr[ position() > 2 ]', table) as Node[];
+    stocks.push({
+      name,
+      type,
+      id,
+      order,
+    });
+  }
 
-    console.log('number of rows', rows.length);
+  console.log("number of stocks", stocks.length);
 
-    const aircraft: IAircraft[] = []
-    for (const row of rows) {
-        // there are blank lines. if the registration is blank, skip this ling
-        if (xpath.select1('.//td[1]', row) == null) {
-            continue;
-        }
+  const result: IOrder = {
+    stocks,
+    order: orderContent,
+  };
 
-        const registration = (xpath.select('.//td[2]', row)[0] as Node).textContent!.trim();
-        const type = (xpath.select('.//td[5]', row)[0] as Node).textContent!.trim();
-        const serialNumber = (xpath.select('.//td[6]', row)[0] as Node).textContent!.trim();
-        const owner = (xpath.select('.//td[7]', row)[0] as Node).textContent!.trim();
-        const operator = (xpath.select('.//td[8]', row)[0] as Node).textContent!.trim();
-
-        aircraft.push({
-            registration,
-            type,
-            serialNumber,
-            owner,
-            operator,
-            lastSeen: new Date(),
-        });
-    }
-
-    console.log('number of aircraft', aircraft.length);
-
-    const result: IResult = {
-        lastUpdated: new Date(updatedDate[1][0]),
-        aircraft: aircraft,
-    };
-
-    await Deno.writeTextFile('./data.json', JSON.stringify(result, null, 2));
+  await Deno.writeTextFile("./data.json", JSON.stringify(result, null, 2));
+  const sendKey = Deno.env.get("SEND_KEY");
+  var sendConent = "";
+  for (const stock of stocks) {
+    sendConent += `${stock.name}[${stock.id}][${stock.type}]: ${stock.order}\n`;
+  }
+  const sendUrl = encodeURI(
+    `https://sctapi.ftqq.com/${sendKey}.send?title=MONEY&desp=${sendConent}`,
+  );
+  await fetch(sendUrl);
 })();
